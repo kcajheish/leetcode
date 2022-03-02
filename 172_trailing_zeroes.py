e5b4eacb343fd933503a949f41128f9cbef07f7e
jackhsieh <jackhsien2002@gmail.com>
2022-03-02 13:04:09 +0800

    lc 172 comment, enhance

--- 172_trailing_zeroes.py
+++ 172_trailing_zeroes.py
@@ -19,21 +19,41 @@ https://leetcode.com/problems/factorial-trailing-zeroes/discuss/52373/Simple-CC%
 
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        num_of_2 = 0
+        """
+        thought:
+        find out number of factor 5 each integer from 1 to n
+        use memo to remember results
+
+        problems:
+        a lot of wasted calculation on number that are not factor of five
+
+        time complexity: amortized O(n)
+        """
         num_of_5 = 0
-        memo_2 = dict()
-        memo_5 = dict()
         for i in range(1, n+1):
-            num_of_2 += self.factor(i, 2, memo_2)
-            num_of_5 += self.factor(i, 5, memo_5)
-        return min(num_of_2, num_of_5)
+            num_of_5 += self.factor(i, 5, {})
+        return num_of_5
 
     def factor(self, n, f, memo):
         if n in memo:
             return memo[n]
+
         if n % f == 0:
             res = 1 + self.factor(n/f, f, memo)
         else:
             res = 0
         memo[n] = res
         return memo[n]
+
+    def trailingZeroesv2(self, n):
+        """
+        thought:
+        start with solution, assume we look for number that has factor of 5, 5*5, 5*5*5...
+        time complexity: O(n)
+        """
+        factor = 5
+        res = 0
+        while factor <= n:
+            res += n//factor
+            factor *= 5
+        return res
